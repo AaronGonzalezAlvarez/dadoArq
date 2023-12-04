993d16160dd79f27c84307100b183a0c4b7401a8
DAW_M <aaroooooon060812@gmail.com>
2023-12-04 13:51:23 +0100

    no arranca, el motivo era porque le estaba pasado un numero en ves de la iamgen

--- app/src/main/java/com/example/dadoarq/ui/dado/ui/DadoUiState.kt
+++ app/src/main/java/com/example/dadoarq/ui/dado/ui/DadoUiState.kt
@@ -1,6 +1,8 @@
 package com.example.dadoarq.ui.dado.ui
 
+import com.example.dadoarq.R
+
 data class DadoUiState(
-    val num:Int = 1,
+    val num:Int = R.drawable.dice_1,
     val numReal:Int = 1
 )
